b722dfa212090c0d48469f8611ffec104f9e59a2
Stanton <stantonburton@gmail.com>
2020-11-12 13:38:16 -0600

    Got rollup config working

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.svelte';
-
+import 'blueprint-css/dist/blueprint.min.css';
 const app = new App({
 	target: document.body
 });
-
+// let's merge this comment.
 export default app;
